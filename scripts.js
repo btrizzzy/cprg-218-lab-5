31c077a607aed52b3e1c1400304147e96a54d552
btrizzzy <147527808+btrizzzy@users.noreply.github.com>
2024-04-03 13:45:16 -0600

    feat: add dropdown & button

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
+/* Create character card */
+function createCard() {
 
-async function fetchCharacters() {
-  try {
-    const response = await fetch('https://ghibliapi.vercel.app/people');
-    const data = await response.json();
-    return data.results;
-    console.log(data.results);
-    
-    //Error handling
-    } catch (error) {
-    console.log('Error');
-    }
 }
+
+/* Get details of Ghibli characters */
+async function getGhibli() {
+  try {
+    const response = await fetch("https://ghibliapi.vercel.app/people/");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/* Populate dropdown list*/
+async function renderDetails(result) {
+  const card = createCardElement({
+    title: data.name,
+    subtitle: data.types.map((type) => type.type.name).join(", "),
+    image: data.sprites.other["official-artwork"].front_default,
+  });
+}
+
+
+getGhibli().then((results) => console.log(results));
